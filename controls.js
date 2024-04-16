aec534fdf92d2a249540ef6c496ca7770178edd6
InceFox <167045725+InceFox@users.noreply.github.com>
2024-04-16 15:14:21 -0300

    Update controls.js
    atualização para entrega final

--- controls.js
+++ controls.js
@@ -1,6 +1,6 @@
 export default {
     jump: new KeyboardEvent('keydown', {key: 'Space', keyCode: 32}),
     dispatch(event) {
-        document.dispatchEvent(this[event]);
+        document.dispatchEvent(this.jump);
     }
 }
